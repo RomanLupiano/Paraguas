08ef5ba58fb4a3c83ca024b62a1a365f0b8c4225
RomanLupiano <romanlupiano@gmail.com>
2022-08-31 19:01:59 -0300

    Consumo de API

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
 from flask import Flask, render_template, request
 import requests
-
-API_KEY = '1350767293eef6c07127ff209dd93d7a'
+import configparser
 
 app = Flask('__name__')
 
-def get_weather_data(city_name):
-    return 
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'GET':
-        city = request.form.get('Malaga')
-        data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric')
+    if request.method == 'POST':
+        city = request.form.get('city')
 
-        json_object = data.json()
+        if city == '':
+            return render_template('home.html')
 
-        temp = float(json_object['main']['temp'])
-        return render_template('home.html', temperature=temp)
+        data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={get_api_key()}&units=metric').json()
+
+        return render_template('home.html', 
+        temperature=data['main']['temperature'],
+        feels_like=data['main']['feels_like'],
+        pressure=data['main']['pressure'],
+        humidity=data['main']['humidity'],
+        Feels_like=data['main']['humidity'])
     else:
-        return render_template('home.html')
+        return render_template('home.html', data='')
+
+@app.route('/find-city/<lat>/<lon>/')
+def get_url_data(lat, lon):
+    data = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={get_api_key()}&units=metric').json()
+    return render_template('home.html', 
+        temperature=data['current']['temp'],
+        feels_like=data['current']['feels_like'],
+        pressure=data['current']['pressure'],
+        humidity=data['current']['humidity'],
+        clouds=data['current']['clouds'],
+        wind_speed=data['current']['wind_speed'],
+        hourly_pop=data['hourly'],
+        daily_pop=data['daily'])
 
 @app.route('/about')
 def about():
