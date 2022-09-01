affbb1c87766c01776c7871cc14aaea62c1e66d2
RomanLupiano <romanlupiano@gmail.com>
2022-09-01 19:51:09 -0300

    Mejora de alineamiento

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
         data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={get_api_key()}&units=metric').json()
 
         return render_template('home.html', 
-        temperature=data['main']['temperature'],
+        temperature=data['main']['temp'],
         feels_like=data['main']['feels_like'],
         pressure=data['main']['pressure'],
         humidity=data['main']['humidity'],
@@ -31,15 +31,23 @@ def home():
 @app.route('/find-city/<lat>/<lon>/')
 def get_url_data(lat, lon):
     data = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={get_api_key()}&units=metric').json()
+    temperature = data['current']['temp']
+    if temperature < 20:
+        answer1 = 'yes'
+        answer2 = 'yes'
+
+    
     return render_template('home.html', 
-        temperature=data['current']['temp'],
+        temperature=temperature,
         feels_like=data['current']['feels_like'],
         pressure=data['current']['pressure'],
         humidity=data['current']['humidity'],
         clouds=data['current']['clouds'],
         wind_speed=data['current']['wind_speed'],
-        hourly_pop=data['hourly'],
-        daily_pop=data['daily'])
+        hourly_pop=data['hourly'][0]['pop'],
+        daily_pop=data['daily'][0]['pop'],
+        answer1=answer1,
+        answer2=answer2)
 
 @app.route('/about')
 def about():
